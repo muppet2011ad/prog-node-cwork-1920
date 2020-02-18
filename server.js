9344c4fffe54012ad307e46ad1d8547b311b26df
James Townsend <j.a.townsend2000@gmail.com>
2020-02-18 12:38:28 +0000

    Now possible to add spells to array - does take some time for request to complete though

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var fetch = require('node-fetch');
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var spells = require('./data/spells.json');
 var characters = require('./data/characters.json')
@@ -13,7 +14,7 @@ app.get('/', function (req, resp) {
     resp.send('Hello, World!');
 });
 
-app.post('/api/admin/addspell', async function (req, resp){
+app.post('/api/admin/addspell', function (req, resp){
     try {
         let newspell = {
             Name: req.body.Name,
@@ -22,23 +23,29 @@ app.post('/api/admin/addspell', async function (req, resp){
             Components: req.body.Components,
             Damage: req.body.Damage,
             Desc: req.body.Desc
+        };
+        if (newspell.Name == undefined){
+            resp.status(400).send();
+            return;
         }
         spells.push(newspell);
-        let json = await JSON.stringify(spells);
+        let json = JSON.stringify(spells);
         fs.writeFile('./data/spells.json', json, 'utf8', () => {});
-        resp.status(400).send();
+        resp.status(200).send();
     } catch (e) {
+        console.log(e);
         resp.status(500).send(e);
     }
 });
 
-app.post('/api/admin/addspell', async function (req, resp) {
+app.post('/api/admin/addspells', function (req, resp) {
     try {
         spells.concat(req.body.arr);
-        let json = await JSON.stringify(spells);
+        let json = JSON.stringify(spells);
         fs.writeFile('./data/spells.json', json, 'utf8', () => {});
-        resp.status(400).send();
+        resp.status(200).send();
     } catch (e) {
+        console.log(e);
         resp.status(500).send();
     }
 })
