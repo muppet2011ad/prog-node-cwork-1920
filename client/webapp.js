7f9fad0896f491e99cdf677832c0dd1354f0642c
James Townsend <j.a.townsend2000@gmail.com>
2020-02-26 17:40:09 +0000

    New characters can now be created on the client

--- client/webapp.js
+++ client/webapp.js
@@ -1,21 +1,13 @@
 let characters;
 
-async function start(){ // Setup function
+let auth = localStorage.getItem("auth");
+
+async function getAllChars(){ // Setup function
     const response = await fetch("http://localhost:8090/api/characters", {
         method: "GET",
         headers: new Headers({"Authorization": localStorage.getItem("auth")}),
     }); // Fetch all characters
     characters = await response.json(); // Json it up
-    makeCharList(characters); // Display it
-}
-
-document.getElementById("charsearch").onsubmit = async function (event) {
-    event.preventDefault(); // We don't want to actually GET since that switches page
-    const response = await fetch("http://localhost:8090/api/characters?name=" + document.getElementById("name").value, {
-        method: "GET",
-        headers: new Headers({"Authorization": localStorage.getItem("auth")}),
-    }); // Complete the user's search
-    characters = await response.json();
     makeCharList(characters); // Display it
 }
 
@@ -43,4 +35,56 @@ function makeCharList(chars){
     results.appendChild(newlist); // Display the whole thing (it's better to construct it all then parent the whole thing for performance reasons)
 }
 
-start();
+document.getElementById("charsearch").onsubmit = async function (event) {
+    event.preventDefault(); // We don't want to actually GET since that switches page
+    const response = await fetch("http://localhost:8090/api/characters?name=" + document.getElementById("name").value, {
+        method: "GET",
+        headers: new Headers({"Authorization": localStorage.getItem("auth")}),
+    }); // Complete the user's search
+    characters = await response.json();
+    makeCharList(characters); // Display it
+}
+
+document.getElementById("newCharBtn").onclick = async function (event) {
+    document.getElementById("newCharErrorP").innerHTML = "";
+}
+
+document.getElementById("newCharForm").onsubmit = async function (event) {
+    event.preventDefault();
+    let form = document.getElementById("newCharForm");
+    let formData = new FormData(form);
+    let formJson = {};
+    let incomplete = false;
+    let text = document.getElementById("newCharErrorP");
+    for (const [k, v] of formData.entries()) {
+        formJson[k] = v;
+        if (v == "") {
+            incomplete = true;
+        }
+    }
+    if (incomplete) {
+        text.innerText = "Please complete the form.";
+        return;
+    }
+    const response = await fetch("http://localhost:8090/api/newchar", {
+        method: "POST",
+        headers: new Headers({"Authorization": auth, "Content-Type": "application/json"}),
+        body: JSON.stringify(formJson)
+    });
+    if (response.status == 400) {
+        text.innerText = "Please complete the form.";
+    }
+    else if (response.status == 500) {
+        text.innerText = "Internal Server Error";
+    }
+    else if (response.status == 200) {
+        document.getElementById("newCharModalClose").click();
+        form.reset();
+        getAllChars();
+    }
+    else {
+        text.innerText = "Something went wrong"
+    }
+}
+
+getAllChars();
