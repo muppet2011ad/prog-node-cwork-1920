4c97eb6a6ca93a2e7716b3b9c6d7f2491ba1d452
James Townsend <j.a.townsend2000@gmail.com>
2020-02-26 19:10:37 +0000

    Error messages are now provided in the event that the server is down

--- client/webapp.js
+++ client/webapp.js
@@ -3,12 +3,21 @@ let characters;
 let auth = localStorage.getItem("auth");
 
 async function getAllChars(){ // Setup function
-    const response = await fetch("http://localhost:8090/api/characters", {
-        method: "GET",
-        headers: new Headers({"Authorization": localStorage.getItem("auth")}),
-    }); // Fetch all characters
-    characters = await response.json(); // Json it up
-    makeCharList(characters); // Display it
+    try {
+        const response = await fetch("http://localhost:8090/api/characters", {
+            method: "GET",
+            headers: new Headers({"Authorization": localStorage.getItem("auth")}),
+        }); // Fetch all characters
+        characters = await response.json(); // Json it up
+        makeCharList(characters); // Display it
+    } catch (e) {
+        if (e instanceof TypeError) {
+            alert("Could not reach server. Please try again later.")
+        }
+        else {
+            throw e;
+        }
+    }
 }
 
 function makeCharList(chars){
@@ -36,13 +45,22 @@ function makeCharList(chars){
 }
 
 document.getElementById("charsearch").onsubmit = async function (event) {
-    event.preventDefault(); // We don't want to actually GET since that switches page
-    const response = await fetch("http://localhost:8090/api/characters?name=" + document.getElementById("name").value, {
-        method: "GET",
-        headers: new Headers({"Authorization": localStorage.getItem("auth")}),
-    }); // Complete the user's search
-    characters = await response.json();
-    makeCharList(characters); // Display it
+    try {
+        event.preventDefault(); // We don't want to actually GET since that switches page
+        const response = await fetch("http://localhost:8090/api/characters?name=" + document.getElementById("name").value, {
+            method: "GET",
+            headers: new Headers({"Authorization": localStorage.getItem("auth")}),
+        }); // Complete the user's search
+        characters = await response.json();
+        makeCharList(characters); // Display it
+    } catch (e) {
+        if (e instanceof TypeError) {
+            alert("Could not reach server. Please try again later.")
+        }
+        else {
+            throw e;
+        }
+    }
 }
 
 document.getElementById("newCharBtn").onclick = async function (event) {
@@ -50,40 +68,53 @@ document.getElementById("newCharBtn").onclick = async function (event) {
 }
 
 document.getElementById("newCharForm").onsubmit = async function (event) {
-    event.preventDefault();
-    let form = document.getElementById("newCharForm");
-    let formData = new FormData(form);
-    let formJson = {};
-    let incomplete = false;
-    let text = document.getElementById("newCharErrorP");
-    for (const [k, v] of formData.entries()) {
-        formJson[k] = v;
-        if (v == "") {
-            incomplete = true;
+    try {
+        event.preventDefault();
+        let form = document.getElementById("newCharForm");
+        let formData = new FormData(form);
+        let formJson = {};
+        let incomplete = false;
+        let text = document.getElementById("newCharErrorP");
+        for (const [k, v] of formData.entries()) {
+            formJson[k] = v;
+            if (v == "") {
+                incomplete = true;
+            }
+        }
+        if (incomplete) {
+            text.innerText = "Please complete the form.";
+            return;
+        }
+        const response = await fetch("http://localhost:8090/api/newchar", {
+            method: "POST",
+            headers: new Headers({"Authorization": auth, "Content-Type": "application/json"}),
+            body: JSON.stringify(formJson)
+        });
+        if (response.status == 400) {
+            text.innerText = "Please complete the form.";
+        }
+        else if (response.status == 500) {
+            text.innerText = "Internal Server Error";
+        }
+        else if (response.status == 401) {
+            text.innerText = "Request was unauthorised. Are you logged in?"
+        }
+        else if (response.status == 200) {
+            document.getElementById("newCharModalClose").click();
+            form.reset();
+            getAllChars();
+        }
+        else {
+            text.innerText = "Something went wrong"
         }
     }
-    if (incomplete) {
-        text.innerText = "Please complete the form.";
-        return;
-    }
-    const response = await fetch("http://localhost:8090/api/newchar", {
-        method: "POST",
-        headers: new Headers({"Authorization": auth, "Content-Type": "application/json"}),
-        body: JSON.stringify(formJson)
-    });
-    if (response.status == 400) {
-        text.innerText = "Please complete the form.";
-    }
-    else if (response.status == 500) {
-        text.innerText = "Internal Server Error";
-    }
-    else if (response.status == 200) {
-        document.getElementById("newCharModalClose").click();
-        form.reset();
-        getAllChars();
-    }
-    else {
-        text.innerText = "Something went wrong"
+    catch (e) {
+        if (e instanceof TypeError) {
+            document.getElementById("newCharErrorP").innerText = "Could not reach server. Please try again later."
+        }
+        else{
+            throw e;
+        }
     }
 }
 
