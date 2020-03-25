b17f014724573b9bb18856cd582df24ac797781b
James Townsend <j.a.townsend2000@gmail.com>
2020-03-25 18:44:36 +0000

    Initial tests for /api/newchar written

--- __tests__/app-test.js
+++ __tests__/app-test.js
@@ -64,6 +64,37 @@ describe('Test user creation and subsequent authentication', () => {
   });
 });
 
+describe('Character creation and deletion', () => {
+  beforeAll(() => {
+    fs.writeFileSync('./tmp/data/users.json', JSON.stringify([{ Name: 'test', Password: 'password', Chars: [] }]));
+  });
+  test('POST /api/newchar with new character details', () => {
+    return request(app)
+      .post('/api/newchar')
+      .auth('test', 'password')
+      .send({ Name: 'John Smith', Class: 'Fighter', Race: 'Human', Level: 2 })
+      .expect(200) // Verifies request completed successfully
+      .then(response => {
+        const charid = JSON.parse(fs.readFileSync('./tmp/data/users.json'))[0].Chars[0];
+        expect(fs.existsSync('./tmp/data/chars/' + charid + '.json')).toBeTruthy();
+        expect(fs.readFileSync('./tmp/data/charindex.json').includes(charid)).toBeTruthy();
+      });
+  });
+  test('POST /api/newchar with incomplete character details', () => {
+    return request(app)
+      .post('/api/newchar')
+      .auth('test', 'password')
+      .send({ Class: 'Fighter', Race: 'Human', Level: 2 })
+      .expect(400);
+  });
+  test('POST /api/newchar without supplying credentials', () => {
+    return request(app)
+      .post('/api/newchar')
+      .send({ Name: 'John Smith', Class: 'Fighter', Race: 'Human', Level: 2 })
+      .expect(401);
+  });
+});
+
 afterAll(async () => {
   fs.rmdirSync('./tmp/', { recursive: true });
 });
